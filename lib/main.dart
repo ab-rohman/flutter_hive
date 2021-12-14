314171e67fe24ef78f564f2e0a7ea1d6344fe662
Abdul_Rohman <rokhman12abdul@gmail.com>
2021-12-14 21:14:16 +0700

    Create Object and use it

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hive/mapel.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 late Box box;
 Future<void> main() async {
   await Hive.initFlutter();
   box = await Hive.openBox('box');
-  Hive.registerAdapter(MyObjectAdapter());
+  Hive.registerAdapter(MapelAdapter());
+  box.put('mapel', Mapel(title: "Mobile", kelas: "Link Kelas"));
   runApp(const MyApp());
 }
 
@@ -14,6 +16,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    Mapel mapel = box.get('mapel');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hive data"),
+      ),
+      body: Text("${mapel.title} ${mapel.kelas}"),
+    );
   }
 }
